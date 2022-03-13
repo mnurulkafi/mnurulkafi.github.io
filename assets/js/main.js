3f5e921c7874ac6f21b541ba381d5f93a6c48794
Moch Nurul Kafi <nurulkafi18@gmail.com>
2022-03-13 16:25:45 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    $('.bi-moon-fill').hide();
+    particle_dark();
+    let body = "dark";
+    $('#flexSwitchCheckDefault').click(function(){
+        if(body === "dark"){
+            $('#particles-js').attr('id', 'particles-js-white');
+            particle_white();
+            $('nav').attr('class', 'navbar navbar-expand-lg navbar-light');
+            $('.bi-moon-fill').show();
+            $('.bi-moon').hide();
+            body = "white";
+        }else{
+            $('#particles-js-white').attr('id', 'particles-js');
+            particle_dark();
+            $('nav').attr('class', 'navbar navbar-expand-lg navbar-dark');
+            $('.bi-moon-fill').hide();
+            $('.bi-moon').show();
+            body = "dark";
+        }
+    });
+})
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SERVICES MODAL ===============*/
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/* Link active work */ 
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const particle_dark = () =>{
+    particlesJS("particles-js",
+        {
+            "particles":
+            {
+                "number":
+                {
+                    "value": 50, "density":
+                        { "enable": true, "value_area": 800 }
+                }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } }
+            }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true
+        });
+}
+const particle_white = () =>{
+    particlesJS("particles-js-white",
+        { "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#2f3e49" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#2f3e49", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });;
+}
